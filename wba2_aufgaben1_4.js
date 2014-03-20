7072ba4c7a18926882b1a22769286365382fc511
NRose <niklas.rose@gmx.net>
2014-03-20 14:48:51 +0100

    aufgaben 1-4. noch ein kleiner Fehler bei 4, sonst alles gut

---
+++ wba2_aufgaben1_4.js
@@ -0,0 +1,108 @@
+/*
+**	WEbserver einrichten
+*/	
+
+/**
+Deklaration eines Mehrdim Arrays in dem die Planetennamen, Durchmesser, Entfernung von der Erde stehen
+*/
+planeten = new Array;
+planeten[0] = 	new Array("Erde", 	"Venus", 	"Mars", 	"Jupiter",	"Saturn", 		"Uranus", 		"Neptun", 	"Merkur");
+planeten[1] = new Array("150 Mio", 	"108 Mio", 	"228 Mio", 	"778 Mio", 	"1,433 Mrd", 	"2,872 Mrd", 	"4,495 Mrd","58 Mio");
+planeten[2]= new Array("12734",		"12103",	"6772"	,	"138346",	"114632",		"50532",		"49105",	"4879");
+
+var	http	=	require('http');	
+var	querystring	=	require('querystring');	
+var	url	=	require('url');	
+
+var	server	=	http.createServer();	
+
+server.on('request',	function(req,	res){	
+
+		console.log('HTTP-Request	gestartet')	
+		console.log('HTTP-Methode:	'+req.method);	
+		var	body	=	'';	
+
+		req.on('data',	function(data){	
+				body	=	body	+	data.toString();	
+
+				/**
+				*	Aufgabe 4
+					---------
+				*/
+					if(req.method == 'POST'){	//Überprüfung ob Method(form) POST ist
+						//POST - Daten werden in eine Variable gespeichert(zur Übersicht)
+						//und an das PlanetenArray dran gehangen
+						var planetName = req.body.name;		
+						var planetDiameter = req.body.diameter;
+						var planetDistance = req.body.distance;
+
+						planeten[0].push(planetName);
+						planeten[1].push(planetDistance);
+						planeten[2].push(planetDiameter);
+
+						
+					}
+		});	
+
+		req.on('end',	function(){	
+				console.log('HTTP-Request	zu	Ende');	
+				var	daten	=	querystring.parse(body);	
+				var	pfad	=	url.parse(req.url).pathname;
+				
+				/**
+					Aufgabe 2
+					---------
+
+					Abfrage auf die URL 
+				*/
+				if(pfad == '/planeten'){
+
+					console.log('Pfad:	'+pfad);	
+					console.log(daten);	
+
+					res.writeHead(200,	"OK",	{'Content-Type':	'text/html'});	
+					//res.write('Hallo	'+	daten.name);
+					res.write("<html><body>");
+					res.write("<table>"); //erstellt in einem HTML Dokument ein Table-Tag
+
+					for (var i = 0; i < planeten.length; i++){	//schleife für jeweils eine Zeile durch hinzufügen des TR-Tags (HTML)
+						res.write("<tr>");
+						for (var j = 0; j < planeten[0].length ; j++){ //schleife für die Inhalte einer Zeile
+							res.write("<td>"+planeten[i][j]+"</td>")
+						}
+						res.write("</tr>"); 
+					}
+					res.write("<table>"); //table wird geschlossen
+
+					/**
+					*	Aufgabe 3
+						---------
+
+						Erstellung eines Formulares 
+					*/
+
+					res.write("<form action='http://localhost:8888/planeten' method='POST'>");
+					res.write("<br><br><br>Planetennamen: <input type='text' name='name'> <br>");
+					res.write("Planetdurchmesser: <input type='text' name='diameter'> <br>");
+					res.write("Entfernung: <input type='text' name='distance'> <br>");
+					res.write("<input type='submit' value='SEND' >");
+					res.write("</form>");
+
+					res.write("</html></body>");
+
+
+				}
+				res.end();	
+		});	
+
+		/**
+	Aufgabe 1
+*/
+
+
+
+});	
+
+server.listen(8888);
+
+
