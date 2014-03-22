7b339c62bbe51baf275a86aa21c15d361207c147
NRose <niklasrose89@googlemal.com>
2014-03-22 21:31:49 +0100

    now it's more beautiful. Comments, space between lines, etc.

--- wba2_aufgaben1_4.js
+++ wba2_aufgaben1_4.js
@@ -1,7 +1,3 @@
-/*
-**	WEbserver einrichten
-*/	
-
 /**
 Deklaration eines Mehrdim Arrays in dem die Planetennamen, Durchmesser, Entfernung von der Erde stehen
 */
@@ -10,6 +6,9 @@ planeten[0] = 	new Array("Erde", 	"Venus", 	"Mars", 	"Jupiter",	"Saturn", 		"Ura
 planeten[1] = new Array("150 Mio", 	"108 Mio", 	"228 Mio", 	"778 Mio", 	"1,433 Mrd", 	"2,872 Mrd", 	"4,495 Mrd","58 Mio");
 planeten[2]= new Array("12734",		"12103",	"6772"	,	"138346",	"114632",		"50532",		"49105",	"4879");
 
+/*
+*	Webserver einrichten
+*/	
 var	http	=	require('http');	
 var	querystring	=	require('querystring');	
 var	url	=	require('url');	
@@ -32,18 +31,19 @@ server.on('request',	function(req,	res){
 				var	pfad	=	url.parse(req.url).pathname;
 				
 				/**
-					Aufgabe 2
-					---------
-
-					Abfrage auf die URL 
+				*	Aufgabe 2
+				*	--------- 
 				*/
-				if(pfad == '/planeten'){
-
+				if(pfad == '/planeten'){	//korrekter Pfad wird verglichen mit der Eingabe
 					console.log('Pfad:	'+pfad);	
 					console.log(data);	
 
 					res.writeHead(200,	"OK",	{'Content-Type':	'text/html'});	
-					//res.write('Hallo	'+	daten.name);
+
+					/**
+					*	Aufgabe 1
+					*	---------
+					*/
 					res.write("<html><body>");
 					res.write("<table>"); //erstellt in einem HTML Dokument ein Table-Tag
 
@@ -54,7 +54,7 @@ server.on('request',	function(req,	res){
 						}
 						res.write("</tr>"); 
 					}
-					res.write("<table>"); //table wird geschlossen
+					res.write("</table>"); //table wird geschlossen
 
 					/**
 					*	Aufgabe 3
@@ -62,14 +62,12 @@ server.on('request',	function(req,	res){
 
 						Erstellung eines Formulares 
 					*/
-
 					res.write("<form action='http://localhost:8888/planeten' method='POST'>");
 					res.write("<br><br><br>Planetennamen: <input type='text' name='planet'> <br>");
 					res.write("Planetdurchmesser: <input type='text' name='diameter'> <br>");
 					res.write("Entfernung: <input type='text' name='distance'> <br>");
 					res.write("<input type='submit' value='SEND' >");
 					res.write("</form>");
-
 					res.write("</html></body>");
 
 					/**
@@ -85,24 +83,12 @@ server.on('request',	function(req,	res){
 
 						planeten[0].push(planetName);
 						planeten[1].push(planetDistance);
-						planeten[2].push(planetDiameter);
-
-						
+						planeten[2].push(planetDiameter);	
 					}
-
-
 				}
 				res.end();	
 		});	
-
-		/**
-	Aufgabe 1
-*/
-
-
-
 });	
-
 server.listen(8888);
 
 
